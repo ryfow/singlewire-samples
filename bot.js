78258f9f8e35c63b0e5626fbe90dbf49b378bc9a
Glitch (ryfow-singlewire-samples) <none>
2019-04-18 22:29:35 +0000

    👬⛹ Checkpoint ./bot.js:3363858/9526

--- bot.js
+++ bot.js
@@ -10,11 +10,13 @@ class MyBot {
      */
     async onTurn(turnContext) {
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
+        var text = turnContext.activity.text || "";
+      turnContext.sen
         if (turnContext.activity.type === ActivityTypes.Message) {
-            if ( turnContext.activity.text.toLowerCase().includes('help')) {
+            if ( text.toLowerCase().includes('help')) {
                 await turnContext.sendActivity(`Here's some help!\n\nPress 1 for an incorrectly encoded OpenUrl on iOS`);
             }
-            else if (turnContext.activity.text.includes('1')) {
+            else if (text.includes('1')) {
               var attachment = {
                 "contentType": "application/vnd.microsoft.card.adaptive",
                 "content": {
@@ -37,7 +39,7 @@ class MyBot {
                 }};
                 await turnContext.sendActivity({"type": "message", "attachments" : [attachment]});
             }
-            else if (turnContext.activity.text.includes('1')) {
+            else if (text.includes('2')) {
                 var attachment = {
                   "contentType": "application/vnd.microsoft.card.adaptive",
                   "content": {
@@ -51,17 +53,40 @@ class MyBot {
                             "text": "Here is the adaptive card body"
 
                         },
-                    ],
+                        {
+                            "type": "Input.ChoiceSet",
+                            "id": "CompactSelectVal",
+                            "style": "compact",
+                            "value": "1",
+                            "choices": [
+                              {
+                                "title": "Red",
+                                "value": "1"
+                              },
+                              {
+                                "title": "Green",
+                                "value": "2"
+                              },
+                              {
+                                "title": "Blue",
+                                "value": "3"
+                              }
+                            ]
+                          }],
                     "actions": [
                     {
-                        "type": "Action.OpenUrl",
-                        "url": "https://www.singlewire.com#/that-percent-23-should-be-a-hash",
-                        "title": "OpenUrl"
+                        "type": "Action.Submit",
+                        "title": "Submit",
+                        "data":  {"msteams": {"type": "task/fetch"}}
                     }]
                   }};
                   await turnContext.sendActivity({"type": "message", "attachments" : [attachment]});
               }
+            else if (turnContext.activity.value != null) {
+              await turnContext.sendActivity("Value Submitted: " + JSON.stringify(turnContext.activity.value));
+            }
             else {
+                console.log("activity", turnContext.activity);
                 await turnContext.sendActivity(`You said '${ turnContext.activity.text }'`);
             }
         } else {
